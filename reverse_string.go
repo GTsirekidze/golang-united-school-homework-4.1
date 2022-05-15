dbc3757b4d2912284e2aaca306c477c080fc3a45
GTsirekidze <g_tsirekidze@cu.edu.ge>
2022-05-15 11:12:44 +0400

    ""

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,11 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	r := []rune(input)
+
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+
 	return output
 }
